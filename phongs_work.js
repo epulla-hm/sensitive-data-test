999fbacfc43051f8c56f7e1fad9452995fa336f7
epulla-hm <epulla@happymoney.com>
2024-03-05 16:31:04 -0500

    add: phong's work

---
+++ phongs_work.js
@@ -0,0 +1,15 @@
+// fibonacci function
+
+let fibonacci = function(n) {
+    if (n <= 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// test with python: 
+// results = [fibonacci_of(n) for n in range(15)]
+
+console.log("Results:");
+for (let i = 0; i < 15; i++) {
+    console.log(fibonacci(i));
+}
+
