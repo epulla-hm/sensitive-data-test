d5d5a44d3c43eee35bc53f1782be19d38acf02fa
epulla-hm <epulla@happymoney.com>
2024-03-05 15:46:20 -0500

    update: remove unnecessary comments

--- michaels_work.js
+++ michaels_work.js
@@ -3,7 +3,6 @@ let fibonacci = (n) => {
   return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
-// python: [fibonacci_of(n) for n in range(15)]
 for (let n = 0; n < 15; n++) {
   console.log(fibonacci(n));
 }
